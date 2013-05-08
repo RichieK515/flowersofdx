e7fd5b17e9ef17625c0e035191c8b7534c94dedb
Richie Kim <spree79@gmail.com>
2013-05-08 22:37:48 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flowersofdx::Application.config.secret_token = 'dc2fb2bf8a4e6501645f5734b87bf6bca406e750f0e00f608fbf12512c03d01556e48cead49f83e2ee7dae39213d90343deab188e25ba211c228e2de034cba1f'
